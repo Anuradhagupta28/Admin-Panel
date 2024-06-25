052c205b3fca882f921b379e88cfb4fb9e40296c
Anuradhagupta28 <annugupta8512@gmail.com>
2024-06-25 17:35:14 +0530

    update admin panel

--- src/views/pages/login/Login.js
+++ src/views/pages/login/Login.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { Link } from 'react-router-dom';
 import {
   CButton,
   CCard,
@@ -12,22 +12,46 @@ import {
   CInputGroup,
   CInputGroupText,
   CRow,
-} from '@coreui/react'
-import CIcon from '@coreui/icons-react'
-import { cilLockLocked, cilUser } from '@coreui/icons'
+} from '@coreui/react';
+import CIcon from '@coreui/icons-react';
+import { cilLockLocked, cilUser } from '@coreui/icons';
 
 const Login = () => {
   return (
-    <div className="bg-body-tertiary min-vh-100 d-flex flex-row align-items-center">
-      <CContainer>
+    <div
+      className="min-vh-100 d-flex flex-row align-items-center"
+      style={{ backgroundColor: '#f8f9fa', justifyContent: 'center' }}
+    >
+      <CContainer style={{ maxWidth: '400px' }}>
         <CRow className="justify-content-center">
-          <CCol md={8}>
+          <CCol>
             <CCardGroup>
-              <CCard className="p-4">
-                <CCardBody>
-                  <CForm>
-                    <h1>Login</h1>
-                    <p className="text-body-secondary">Sign In to your account</p>
+              <CCard
+                className="p-4"
+                style={{
+                  borderRadius: '8px',
+                  boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
+                  padding: '2rem',
+                }}
+              >
+                <CCardBody className="d-flex flex-column align-items-center">
+                <img
+                    src="https://dev-v1.solvedudar.com/assets/web/images/logo/logo.png"
+                    alt="Logo"
+                    height={50}
+                    style={{ width: 'auto', marginBottom: '1.5rem' }}
+                  />
+                  <h2 className="text-center" >
+                    Admin - Login
+                  </h2>
+                 
+                  <p
+                    className="text-body-secondary text-center"
+                    style={{ color: '#6c757d', marginBottom: '2rem',marginTop: '-0.3rem' }} 
+                  >
+                    Sign in to your account
+                  </p>
+                  <CForm style={{ width: '100%', maxWidth: '320px' }}>
                     <CInputGroup className="mb-3">
                       <CInputGroupText>
                         <CIcon icon={cilUser} />
@@ -45,21 +69,31 @@ const Login = () => {
                       />
                     </CInputGroup>
                     <CRow>
-                      <CCol xs={6}>
+                      <CCol className="d-flex justify-content-center mb-3">
                         <CButton color="primary" className="px-4">
                           Login
                         </CButton>
                       </CCol>
-                      <CCol xs={6} className="text-right">
+                    </CRow>
+                    <CRow>
+                      <CCol className="d-flex justify-content-center">
                         <CButton color="link" className="px-0">
                           Forgot password?
                         </CButton>
                       </CCol>
+                     
                     </CRow>
+                    <p
+                    className="text-body-secondary text-center"
+                    style={{ fontSize:'13px', color: '#6c757d', marginBottom: '2rem' }}
+                  >
+                   ©2024 Dudar Tech Pvt. Ltd. All Rights Reserved.
+                  </p>
+             
                   </CForm>
                 </CCardBody>
               </CCard>
-              <CCard className="text-white bg-primary py-5" style={{ width: '44%' }}>
+              {/* <CCard className="text-white bg-primary py-5" style={{ width: '44%' }}>
                 <CCardBody className="text-center">
                   <div>
                     <h2>Sign up</h2>
@@ -74,13 +108,13 @@ const Login = () => {
                     </Link>
                   </div>
                 </CCardBody>
-              </CCard>
+              </CCard> */}
             </CCardGroup>
           </CCol>
         </CRow>
       </CContainer>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
